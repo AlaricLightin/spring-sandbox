916d8b3f4c30a7946196315027f48025eeef1b4f
Alexander Biderman <a_lightin@mail.ru>
2020-07-29 17:02:13 +0300

    Unsuccessful try to load xml into DB PostgreSQL with liquibase.

---
+++ postgres-liquibase-xml-loading/src/main/java/com/example/postgresliquibasexmlloading/PostgresLiquibaseXmlLoadingApplication.java
@@ -0,0 +1,13 @@
+package com.example.postgresliquibasexmlloading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgresLiquibaseXmlLoadingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostgresLiquibaseXmlLoadingApplication.class, args);
+    }
+
+}
---
+++ postgres-liquibase-xml-loading/src/test/java/com/example/postgresliquibasexmlloading/PostgresLiquibaseXmlLoadingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.postgresliquibasexmlloading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostgresLiquibaseXmlLoadingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
